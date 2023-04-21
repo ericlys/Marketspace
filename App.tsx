e6244bf253a57113708f321dcf80ee8e03c41293
Ericlys Moreira <ericlysm@gmail.com>
2023-04-21 19:29:24 -0300

    chore: karla fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Karla_400Regular,
+    Karla_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <Text>Loading ...</Text>;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>

c0e3b999bb354fd23ad9d3c12d2f2e259be60be0
Ericlys Moreira <ericlysm@gmail.com>
2023-04-21 19:37:34 -0300

    chore: setup statusbar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import { useFonts, Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla'
 
 export default function App() {
@@ -13,9 +12,12 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
+      <StatusBar 
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
     </View>
   );
 }

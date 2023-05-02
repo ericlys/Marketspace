43d2a5eca4fc93f45865ee025ff1901704d1c029
Ericlys Moreira <ericlysm@gmail.com>
2023-05-02 20:40:20 -0300

    feat: finish signIn

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,6 +1,7 @@
-import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
-import { Box, useTheme } from "native-base";
-import { AuthRoutes } from "./auth.routes";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native"
+import { Box, useTheme } from "native-base"
+import { AuthRoutes } from "./auth.routes"
+import { AuthContextProvider } from "@contexts/AuthContext"
 
 export function Routes(){
   const { colors } = useTheme()
@@ -11,7 +12,9 @@ export function Routes(){
   return(
     <Box flex={1} bg="gray.200">
       <NavigationContainer theme={theme}>
-        <AuthRoutes/>
+        <AuthContextProvider>
+          <AuthRoutes/>
+        </AuthContextProvider>
       </NavigationContainer>
     </Box>
   )
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,19 +1,59 @@
-import { Center, Image, Text, VStack } from "native-base";
-import LogoPng from '@assets/logo.png';
-import AppName from '@assets/marketspace.png';
-import { Input } from "@components/Input";
-import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
-import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { Center, Image, Text, VStack, useToast } from "native-base"
+import LogoPng from '@assets/logo.png'
+import AppName from '@assets/marketspace.png'
+
+import { Input } from "@components/Input"
+import { Button } from "@components/Button"
+import { useNavigation } from "@react-navigation/native"
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes"
+
+import { Controller, useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from 'yup'
+import { useAuth } from "@hooks/useAuth"
+import { AppError } from "@utils/AppError"
+
+type FormDataProps = {
+  email: string
+  password: string
+}
+
+const signInSchema = yup.object({
+  email: yup.string().required('Informe o e-mail').email('E-mail inválido.'),
+  password: yup.string().required('Informe a senha').min(6, 'A senha deve conter pelo menos 6 dígitos.')
+})
+
 
 export function SignIn() {
-  const navigation = useNavigation<AuthNavigatorRoutesProps>();
-
+  const navigation = useNavigation<AuthNavigatorRoutesProps>()
+  const { signIn } = useAuth()
+  const toast = useToast()
 
   function handleNewAccount() {
     navigation.navigate('signUp')
   }
 
+  const { control, handleSubmit, formState: { errors, isLoading } } = useForm<FormDataProps>({
+    resolver: yupResolver(signInSchema)
+  })
+
+  async function handleSignIn({email, password}: FormDataProps) {
+    try {
+      
+      await signIn(email, password)
+
+    } catch (err) {
+      const isAppError = err instanceof AppError
+      const title = isAppError ? err.message : 'Não foi possível entrar. Tente novamente mais tarde.'
+    
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+      
+    }     
+  }
 
   return (
     <VStack flex={1} bg="gray.100">
@@ -41,29 +81,57 @@ export function SignIn() {
         </Text>
 
         <Center w="full">
-        <Text 
-          fontFamily="body"
-          color="gray.600"
-          fontSize="sm"
-          mt={20}
-          mb={4}
-        >
-          Acesse sua conta
-        </Text>
+          <Text 
+            fontFamily="body"
+            color="gray.600"
+            fontSize="sm"
+            mt={20}
+            mb={4}
+            style={{
+              textShadowColor: 'rgba(0, 0, 0, 0.25)',
+              textShadowOffset: { width: 0, height: 4 },
+              textShadowRadius: 4,  
+            }}
+          >
+            Acesse sua conta
+          </Text>
 
-          <Input 
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-          <Input 
-            placeholder="Senha"
-            type="password"
-            secureTextEntry={true}
-            autoCapitalize="none"
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value }}) => (
+              <Input 
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                errorMessage={errors.email?.message}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
 
-          <Button title="Entrar" />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value }}) => (
+              <Input 
+                placeholder="Senha"
+                type="password"
+                autoCapitalize="none"
+                secureTextEntry
+                errorMessage={errors.password?.message}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Button 
+            title="Entrar"
+            isLoading={isLoading}
+            onPress={handleSubmit(handleSignIn)}
+          />
         </Center>
       </Center>
 
